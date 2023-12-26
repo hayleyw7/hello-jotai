8357ac0c8b822b41b576a534509a51b8480b77ff
Hayley Witherell <hayleywitherell@gmail.com>
2023-12-26 14:59:58 -0700

    Add theme switcher

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,31 @@ import './App.css';
 import { atom, useAtom } from 'jotai';
 
 const counter = atom(0);
+const theme = atom('dark');
 
 export default function Page() {
   const [count, setCounter] = useAtom(counter);
+  const [appTheme, setAppTheme] = useAtom(theme);
+
   const onClick = () => setCounter(prev => prev + 1);
+
+  const handleClick = () => {
+    setAppTheme(appTheme === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${appTheme}`}>
       <header className="App-header">
-        <h1>{count}</h1>
-        <button onClick={onClick}>Click</button>
+        <section>
+          <h1>Incrementer: {count}</h1>
+          <button onClick={onClick}>+1</button>
+        </section>
+
+        <section>
+          <h1>Theme Switcher</h1>
+          <button onClick={handleClick}>{appTheme === 'light' ? 'DARK' : 'LIGHT'}</button>
+        </section>
       </header>
     </div>
-  )
+  );
 }
