4b95070a29e087ab43091f65d44025970292a57d
Hayley Witherell <hayleywitherell@gmail.com>
2023-12-26 17:46:39 -0700

    Update theme toggle

--- src/components/features/ThemeSwitcher.js
+++ src/components/features/ThemeSwitcher.js
@@ -6,7 +6,7 @@ export default function Theme({ appTheme, onThemeSwitchClick }) {
   return (
     <section>
       <h1>Theme Switcher</h1>
-      <button onClick={handleClick}>{appTheme === 'light' ? 'DARK' : 'LIGHT'}</button>
+      <button onClick={handleClick}>{appTheme === 'dark' ? 'LIGHT' : 'DARK'}</button>
     </section>
   );
 }
